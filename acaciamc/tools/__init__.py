7122235042428f75f952c565ac2ba5b9985e0f0b
CBerJun <cberjun@163.com>
2023-07-28 02:41:38 +0800

    Add tool `method_of`

--- acaciamc/tools/__init__.py
+++ acaciamc/tools/__init__.py
@@ -1,1 +1,16 @@
 """Acacia tools for creating binary modules."""
+
+from typing import Callable as _Callable
+
+import acaciamc.mccmdgen.expression as _acacia
+
+def method_of(expr: "_acacia.AcaciaExpr", name: str):
+    """Return a decorator that defines a method for `expr` with `name`
+    whose implementation is decorated function.
+    """
+    def _decorator(func: _Callable):
+        expr.attribute_table.set(
+            name, _acacia.BinaryFunction(func, expr.compiler)
+        )
+        return func
+    return _decorator
