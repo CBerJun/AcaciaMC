16c9b53e09c7a2685fb4314ed1274a755b80e101
CBerJun <cberjun@163.com>
2024-04-24 22:09:52 -0400

    Fix bugs related to instance creation
    Fix that when unable to instantiate in compile time functions, the type
    shown in error message is always "type".
    
    Fix that when __new__ is only available in either of runtime or compile
    time, unexpected error occurs when calling in the one that it does not
    support. Now a CANT_CREATE_INSTANCE error is raised. A way to reproduce
    is to call `schedule.Task` inside a compile time function.

--- acaciamc/mccmdgen/expression/types.py
+++ acaciamc/mccmdgen/expression/types.py
@@ -8,7 +8,6 @@ from typing import TYPE_CHECKING
 from abc import ABCMeta, abstractmethod
 
 from .base import *
-from .none import NoneDataType, ctdt_none
 from acaciamc.error import *
 from acaciamc.mccmdgen.datatype import DefaultDataType
 from acaciamc.ctexec.expr import CTDataType, CTCallable
@@ -38,51 +37,22 @@ class Type(ConstExprCombined, AcaciaCallable, CTCallable, metaclass=ABCMeta):
         pass
 
     def call(self, args, keywords):
-        """Calling a type is to create an instance of this type.
-        Two things are done:
-        1. call `self.__new__` and get `instance`
-        2. call `instance.__init__` if exists.
         """
-        # Call `__new__`
+        Calling a type is to create an instance of this type,
+        i.e. calling `__new__`.
+        """
         new = self.attribute_table.lookup('__new__')
-        if new is None:
+        if new is None or not isinstance(new, AcaciaCallable):
             raise Error(ErrorType.CANT_CREATE_INSTANCE,
                         type_=str(self.datatype_hook()))
-        if not isinstance(new, AcaciaCallable):
-            raise TypeError("__new__ of Type objects must be a callable"
-                            " expression")
-        instance, cmds = new.call(args, keywords)
-        # Call initializer of instance if exists
-        initializer = instance.attribute_table.lookup('__init__')
-        if initializer is not None:
-            if not isinstance(initializer, AcaciaCallable):
-                raise TypeError("__init__ of Type objects must be a callable"
-                                " expression")
-            ret, _cmds = initializer.call(args, keywords)
-            if not ret.data_type.matches_cls(NoneDataType):
-                raise TypeError("__init__ of Type objects return %r instead "
-                                "of None" % str(ret.data_type))
-            cmds.extend(_cmds)
-        return instance, cmds
+        return new.call(args, keywords)
 
     def ccall(self, args, keywords):
         new = self.attributes.clookup('__new__')
-        if new is None:
+        if new is None or not isinstance(new, CTCallable):
             raise Error(ErrorType.CANT_CREATE_INSTANCE,
-                        type_=self.cdata_type.name)
-        if not isinstance(new, CTCallable):
-            raise TypeError("__new__ of Type objects must be a callable"
-                            " expression")
-        instance = new.ccall(args, keywords)
-        initializer = instance.attributes.clookup('__init__')
-        if initializer is not None:
-            if not isinstance(initializer, CTCallable):
-                raise TypeError("__init__ of Type objects must be a callable"
-                                " expression")
-            ret = initializer.ccall(args, keywords)
-            if not ctdt_none.is_typeof(ret):
-                raise TypeError("non-None return from __init__")
-        return instance
+                        type_=self.cdatatype_hook().name)
+        return new.ccall(args, keywords)
 
     @abstractmethod
     def datatype_hook(self) -> "DataType":
