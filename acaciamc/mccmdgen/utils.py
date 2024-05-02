c4f3d261540738a8a3b871a2191facb0e6b84bab
CBerJun <cberjun@163.com>
2024-05-02 16:43:56 -0400

    Optimize: add `apply_decorators` function

--- acaciamc/mccmdgen/utils.py
+++ acaciamc/mccmdgen/utils.py
@@ -1,6 +1,8 @@
 """Miscellaneous utility stuffs."""
 
-__all__ = ["InvalidOpError", "unreachable"]
+__all__ = ["InvalidOpError", "unreachable", "apply_decorators"]
+
+from typing import Reversible
 
 class InvalidOpError(Exception):
     """Can be raised in some methods to indicate an invalid operation."""
@@ -10,3 +12,17 @@ def unreachable(message: str = ""):
     """Indicates an unreachable code path."""
     suffix = f": {message}" if message else ""
     raise AssertionError("unreachable() called" + suffix)
+
+def apply_decorators(decorators: Reversible):
+    """
+    Return a decorator that applies a sequence of decorators.
+        @apply_decorators([a, b])
+    is equivalent to
+        @a
+        @b
+    """
+    def decorator(func):
+        for deco in reversed(decorators):
+            func = deco(func)
+        return func
+    return decorator
