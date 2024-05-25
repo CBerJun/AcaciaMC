8d197fbd1e0f1018210761620542f3965dd9ccbe
CBerJun <cberjun@163.com>
2024-05-25 17:29:32 -0400

    Support packaging with setuptools
    Acacia can now be built into wheels with setuptools.
    
    Add several directories related to building into gitignore.

--- acaciamc/__init__.py
+++ acaciamc/__init__.py
@@ -4,3 +4,5 @@ by compiling code into mcfunction commands, supporting variables,
 functions, custom types, arithmetic, entity and coordinate system and
 very rich compile-time structures like strings, lists and maps.
 """
+
+__version__ = "0.1.0"
