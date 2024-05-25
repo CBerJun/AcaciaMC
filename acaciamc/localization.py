2487e7951511f83854bf6796fd873a0dea6aa687
CBerJun <cberjun@163.com>
2024-05-25 11:10:32 -0400

    Remove acaciamc/lang/config.json
    Now the language can be changed through APIs in `localization.py`.

--- acaciamc/localization.py
+++ acaciamc/localization.py
@@ -1,28 +1,40 @@
 """
-为 AcaciaMC 添加本地化支持 (Localization Support)
+Localization Support (本地化支持)
 
-  "代码可读性的一小步, 消除屎山的一大步" - 鲁迅 (其实是creepebucket说的)
+To make sure that `set_language` works well, strings returned by
+`localize` should not be stored. Instead, get a new localized string
+every time. Calling `localize` on module level should be avoided, too.
+
+"代码可读性的一小步, 消除屎山的一大步" - 鲁迅 (其实是creepebucket说的)
 """
 
-__all__ = ["DEFAULT_LANG", "get_lang", "localize", "LocalizedEnum"]
+__all__ = [
+    "DEFAULT_LANGUAGE", "current_language", "set_language",
+    "get_mapping", "localize", "LocalizedEnum"
+]
 
 from typing import Dict
 from enum import Enum
 from pkgutil import get_data
-import json
 
 lang_cache: Dict[str, Dict[str, str]] = {}
+DEFAULT_LANGUAGE = _language = 'en_US'
 
-def load_default_lang():
-    """加载默认语言"""
-    d = get_data(__name__, "lang/config.json")
-    j = json.loads(d)
-    return j["lang"]
+def current_language() -> str:
+    """Get current language code."""
+    return _language
 
-DEFAULT_LANG = load_default_lang()
+def set_language(lang: str) -> None:
+    """Change the language."""
+    global _language
+    _language = lang
 
-def get_lang(lang: str = DEFAULT_LANG) -> Dict[str, str]:
-    """获取指定语言的本地化映射表"""
+def get_mapping(lang: str) -> Dict[str, str]:
+    """
+    Get the mapping that maps from localization key to the text of the
+    specified language.
+    获取指定语言的本地化映射表
+    """
     if lang in lang_cache:
         return lang_cache[lang]
     lang_cache[lang] = lang_file_dict = {}  # 语言映射列表
@@ -37,12 +49,12 @@ def get_lang(lang: str = DEFAULT_LANG) -> Dict[str, str]:
         lang_file_dict[key] = text
     return lang_file_dict
 
-def localize(key: str, lang: str = DEFAULT_LANG) -> str:
-    """
-    Get localized text.
-    获取指定语言的本地化文本
-    """
-    return get_lang(lang)[key]
+def localize(key: str) -> str:
+    """Get localized text under current language."""
+    mapping = get_mapping(_language)
+    if key in mapping:
+        return mapping[key]
+    return get_mapping(DEFAULT_LANGUAGE)[key]
 
 class LocalizedEnum(Enum):
     """
@@ -51,9 +63,6 @@ class LocalizedEnum(Enum):
     localized value using default language.
     """
 
-    def __init__(self, key: str):
-        self._localized = localize(key)
-
     @property
     def localized(self) -> str:
-        return self._localized
+        return localize(self.value)
