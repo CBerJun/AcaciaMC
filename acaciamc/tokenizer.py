CBerJun/AcaciaMC

commit 0f9ef838ba417dcdccc9de13ce0110af7802bd5a

NFC: get rid of a few hacks related to `TokenType`

acaciamc/tokenizer.py
@@ -66,114 +66,142 @@ FONTS = {
     "obfuscated": "k",
 }
 
+class TokenAttr(enum.Enum):
+    """Classification of `TokenType`."""
+
+    # KEYWORD automatically registers this token type as a keyword. The
+    # string value must be a valid Acacia identifier and that is the
+    # keyword registered and displayed to users.
+    KEYWORD = enum.auto()
+    # PUNCTUATION automatically registers an operator/delimiter. The
+    # string value must have length 1 or 2 and must not already mean
+    # something to the tokenizer (e.g. can't register '#' as that's used
+    # for comments). The punctuation itself is also what we display to
+    # users. 2-char punctuations take precedence over 1-char ones when
+    # tokenizing.
+    PUNCTUATION = enum.auto()
+    # SPECIAL tokens are generated by other language constructs (e.g.
+    # indentations). Their string values are displayed to the user
+    # without quotes around them (i.e. INDENT instead of "INDENT").
+    # As a convention, use all uppercase for their string values.
+    SPECIAL = enum.auto()
+
+KEYWORDS: Dict[str, "TokenType"] = {}
+PUNCTUATIONS_1CHAR: Dict[str, "TokenType"] = {}
+PUNCTUATIONS_2CHARS: Dict[str, "TokenType"] = {}
+
 class TokenType(enum.Enum):
     """
-    Token types.
-    Value convention:
-    1. If length of value is 1 or 2 and the first character is not an
-    identifier character ([a-zA-Z0-9_]) or anything special (like '#'),
-    then a token is registered automatically. (e.g. '=' or '->')
-    2. If value is defined between `true` and `false`, then it is
-    registered as a keyword.
+    Types of tokens. If tokens of this kind have payload
+    (`Token.value` is not None), the payload's type is documented using
+    an inline comment of the form `# value: T`.
     """
-    # block
-    indent = 'INDENT'
-    dedent = 'DEDENT'
-    new_line = 'NEW_LINE'
-    lparen = '('
-    rparen = ')'
-    lbracket = '['
-    rbracket = ']'
-    lbrace = '{'
-    rbrace = '}'
-    colon = ':'
-    comma = ','
-    end_marker = 'END_MARKER'  # end of file
-    # command and string
-    command_begin = 'COMMAND_BEGIN'
-    string_begin = 'STRING_BEGIN'
-    text_body = 'TEXT_BODY'  # value: str
-    dollar_lbrace = 'DOLLAR_LBRACE'
-    command_end = 'COMMAND_END'
-    string_end = 'STRING_END'
-    # operator
-    ## binary operators
-    plus = '+'
-    minus = '-'
-    star = '*'
-    slash = '/'
-    mod = '%'
-    ## compare operators
-    equal_to = '=='
-    unequal_to = '!='
-    greater = '>'
-    less = '<'
-    greater_equal = '>='
-    less_equal = '<='
-    ## augmented assign
-    plus_equal = '+='
-    minus_equal = '-='
-    times_equal = '*='
-    divide_equal = '/='
-    mod_equal = '%='
-    ## other
-    equal = '='
-    arrow = '->'
-    point = '.'
-    at = '@'
-    walrus = ':='
-    ampersand = '&'
-    # expressions
-    ## literal
-    integer = 'INTEGER'  # value: int
-    float = 'FLOAT'  # value: float
-    interface_path = 'INTERFACE_PATH'  # value: str
-    ## id
-    identifier = 'IDENTIFIER'  # value: str
-    # keywords
-    true = 'True'  # must at beginning of keyword part
-    def_ = 'def'
-    interface = 'interface'
-    inline = 'inline'
-    entity = 'entity'
-    extends = 'extends'
-    self = 'self'
-    if_ = 'if'
-    elif_ = 'elif'
-    else_ = 'else'
-    while_ = 'while'
-    pass_ = 'pass'
-    and_ = 'and'
-    or_ = 'or'
-    not_ = 'not'
-    return_ = 'return'
-    import_ = 'import'
-    as_ = 'as'
-    from_ = 'from'
-    none = 'None'
-    for_ = 'for'
-    in_ = 'in'
-    struct = 'struct'
-    virtual = 'virtual'
-    override = 'override'
-    const = 'const'
-    static = 'static'
-    new = 'new'
-    false = 'False'  # must at end of keyword part
 
-KEYWORDS: Dict[str, TokenType] = {}
-def _kw_builder():
-    # NOTE this relies on that keywords in `TokenType` enum are all
-    # between `true` and `false`.
-    token_types = tuple(TokenType)
-    for i in range(
-        token_types.index(TokenType.true),
-        token_types.index(TokenType.false) + 1
-    ):
-        token_type = token_types[i]
-        KEYWORDS[token_type.value] = token_type
-_kw_builder()
-del _kw_builder
+    # Structural
+    indent = 'INDENT', TokenAttr.SPECIAL
+    dedent = 'DEDENT', TokenAttr.SPECIAL
+    new_line = 'NEW_LINE', TokenAttr.SPECIAL
+    end_marker = 'END_MARKER', TokenAttr.SPECIAL
+
+    # Brackets
+    lparen = '(', TokenAttr.PUNCTUATION
+    rparen = ')', TokenAttr.PUNCTUATION
+    lbracket = '[', TokenAttr.PUNCTUATION
+    rbracket = ']', TokenAttr.PUNCTUATION
+    lbrace = '{', TokenAttr.PUNCTUATION
+    rbrace = '}', TokenAttr.PUNCTUATION
+
+    # Command and string
+    command_begin = 'COMMAND_BEGIN', TokenAttr.SPECIAL
+    string_begin = 'STRING_BEGIN', TokenAttr.SPECIAL
+    text_body = 'TEXT_BODY', TokenAttr.SPECIAL  # value: str
+    dollar_lbrace = 'DOLLAR_LBRACE', TokenAttr.SPECIAL
+    command_end = 'COMMAND_END', TokenAttr.SPECIAL
+    string_end = 'STRING_END', TokenAttr.SPECIAL
+
+    # Operators
+    plus = '+', TokenAttr.PUNCTUATION
+    minus = '-', TokenAttr.PUNCTUATION
+    star = '*', TokenAttr.PUNCTUATION
+    slash = '/', TokenAttr.PUNCTUATION
+    mod = '%', TokenAttr.PUNCTUATION
+    equal_to = '==', TokenAttr.PUNCTUATION
+    unequal_to = '!=', TokenAttr.PUNCTUATION
+    greater = '>', TokenAttr.PUNCTUATION
+    less = '<', TokenAttr.PUNCTUATION
+    greater_equal = '>=', TokenAttr.PUNCTUATION
+    less_equal = '<=', TokenAttr.PUNCTUATION
+
+    # Delimiters
+    plus_equal = '+=', TokenAttr.PUNCTUATION
+    minus_equal = '-=', TokenAttr.PUNCTUATION
+    times_equal = '*=', TokenAttr.PUNCTUATION
+    divide_equal = '/=', TokenAttr.PUNCTUATION
+    mod_equal = '%=', TokenAttr.PUNCTUATION
+    colon = ':', TokenAttr.PUNCTUATION
+    comma = ',', TokenAttr.PUNCTUATION
+    equal = '=', TokenAttr.PUNCTUATION
+    arrow = '->', TokenAttr.PUNCTUATION
+    point = '.', TokenAttr.PUNCTUATION
+    at = '@', TokenAttr.PUNCTUATION
+    walrus = ':=', TokenAttr.PUNCTUATION
+    ampersand = '&', TokenAttr.PUNCTUATION
+
+    # Literals
+    integer = 'INTEGER', TokenAttr.SPECIAL  # value: int
+    float = 'FLOAT', TokenAttr.SPECIAL  # value: float
+    interface_path = 'INTERFACE_PATH', TokenAttr.SPECIAL  # value: str
+    identifier = 'IDENTIFIER', TokenAttr.SPECIAL  # value: str
+
+    # Keywords
+    true = 'True', TokenAttr.KEYWORD
+    def_ = 'def', TokenAttr.KEYWORD
+    interface = 'interface', TokenAttr.KEYWORD
+    inline = 'inline', TokenAttr.KEYWORD
+    entity = 'entity', TokenAttr.KEYWORD
+    extends = 'extends', TokenAttr.KEYWORD
+    self = 'self', TokenAttr.KEYWORD
+    if_ = 'if', TokenAttr.KEYWORD
+    elif_ = 'elif', TokenAttr.KEYWORD
+    else_ = 'else', TokenAttr.KEYWORD
+    while_ = 'while', TokenAttr.KEYWORD
+    pass_ = 'pass', TokenAttr.KEYWORD
+    and_ = 'and', TokenAttr.KEYWORD
+    or_ = 'or', TokenAttr.KEYWORD
+    not_ = 'not', TokenAttr.KEYWORD
+    return_ = 'return', TokenAttr.KEYWORD
+    import_ = 'import', TokenAttr.KEYWORD
+    as_ = 'as', TokenAttr.KEYWORD
+    from_ = 'from', TokenAttr.KEYWORD
+    none = 'None', TokenAttr.KEYWORD
+    for_ = 'for', TokenAttr.KEYWORD
+    in_ = 'in', TokenAttr.KEYWORD
+    struct = 'struct', TokenAttr.KEYWORD
+    virtual = 'virtual', TokenAttr.KEYWORD
+    override = 'override', TokenAttr.KEYWORD
+    const = 'const', TokenAttr.KEYWORD
+    static = 'static', TokenAttr.KEYWORD
+    new = 'new', TokenAttr.KEYWORD
+    false = 'False', TokenAttr.KEYWORD
+
+    attr: TokenAttr
+
+    def __new__(cls, s: str, attr: TokenAttr):
+        obj = object.__new__(cls)
+        obj._value_ = s
+        obj.attr = attr
+        if attr is TokenAttr.KEYWORD:
+            KEYWORDS[s] = obj
+        elif attr is TokenAttr.PUNCTUATION:
+            if len(s) == 1:
+                d = PUNCTUATIONS_1CHAR
+            else:
+                assert len(s) == 2
+                d = PUNCTUATIONS_2CHARS
+            d[s] = obj
+        else:
+            assert attr is TokenAttr.SPECIAL
+        return obj
 
 BRACKETS = {
     TokenType.lparen: TokenType.rparen,
@@ -208,6 +236,7 @@ class Token(NamedTuple):
         return (self.pos1, self.pos2)
 
     def __repr__(self) -> str:
+        # For debugging purpose only
         if self.value is None:
             value_str = ''
         else:
@@ -219,11 +248,11 @@ class Token(NamedTuple):
 
     def display_string(self) -> str:
         """Get a string representation to display to user."""
-        v = self.type.value
-        if not v.isupper():
-            # Add quotes to symbols or keywords
-            v = repr(v)
-        return v
+        attr = self.type.attr
+        if attr is TokenAttr.SPECIAL:
+            return self.type.value
+        assert attr in (TokenAttr.KEYWORD, TokenAttr.PUNCTUATION)
+        return repr(attr)  # Wrap with single quotes
 
 class _FormattedStrManager:
     """
@@ -397,12 +426,12 @@ class Tokenizer:
                     res.extend(self.handle_command())
             if self.string_stack and not self.in_string_fexpr:
                 res.extend(self.handle_string())
-            # skip spaces
+            # Skip spaces
             self.skip_spaces()
-            # check end of line
+            # Check end of line
             if self.current_char in ("\n", "\\", None):
                 break
-            # comment
+            # Comment
             if self.current_char == "#":
                 if self.peek() == "*":
                     self.continued_comment_pos = self.current_pos
@@ -413,8 +442,7 @@ class Tokenizer:
                 else:
                     self.skip_comment()
                     break
-            # start
-            ## path after `interface` keyword
+            # Path after `interface` keyword
             if self.last_is_interface:
                 if self.current_char == '"':
                     # Quoted path
@@ -426,7 +454,7 @@ class Tokenizer:
                     res.append(self.handle_interface_path())
                 self.last_is_interface = False
                 continue
-            ## special tokens
+            # Special tokens
             ok = True
             if self.current_char in digits:
                 res.append(self.handle_number())
@@ -455,31 +483,20 @@ class Tokenizer:
                 ok = False
             if ok:
                 continue
-            ## try two-char token
+            # Try two-char punctuation
             peek = self.peek()
             if peek is not None:
-                twochars = self.current_char + peek
-                try:
-                    token_type = TokenType(twochars)
-                except ValueError:
-                    pass
-                else:
-                    # It is indeed a two-char token!
+                two_chars = self.current_char + peek
+                token_type = PUNCTUATIONS_2CHARS.get(two_chars)
+                if token_type is not None:
                     with self.make_token(token_type) as gettok:
                         self.forward()
                         self.forward()
                     res.append(gettok())
                     continue
-            ## try one-char token
-            try:
-                token_type = TokenType(self.current_char)
-            except ValueError:
-                # We've run out of possibilities.
-                self.error_range(
-                    'invalid-char', source_range_from_pos(self.current_pos, 1),
-                    args={'char': STStr(self.current_char)}
-                )
-            else:
+            # Try one-char punctuation
+            token_type = PUNCTUATIONS_1CHAR.get(self.current_char)
+            if token_type is not None:
                 if token_type in BRACKETS:
                     self.bracket_stack.append(_BracketFrame(
                         token_type, self.current_pos
@@ -511,6 +528,12 @@ class Tokenizer:
                 with self.make_token(token_type) as gettok:
                     self.forward()
                 res.append(gettok())
+                continue
+            # We've run out of possibilities, throw
+            self.error_range(
+                'invalid-char', source_range_from_pos(self.current_pos, 1),
+                args={'char': STStr(self.current_char)}
+            )
         # Now self.current_char is either '\n', '\\' or None (EOF)
         if (
             # ${} in single line command can't use implicit line continuation.

commit 07b1f745385528bd2326b509551936fba7c920c3

Fix unexpected-token diagnostic message
A bug caused by commit 0f9ef83.

acaciamc/tokenizer.py
@@ -252,7 +252,7 @@ class Token(NamedTuple):
         if attr is TokenAttr.SPECIAL:
             return self.type.value
         assert attr in (TokenAttr.KEYWORD, TokenAttr.PUNCTUATION)
-        return repr(attr)  # Wrap with single quotes
+        return repr(self.type.value)  # Wrap with single quotes
 
 class _FormattedStrManager:
     """